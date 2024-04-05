6a37ab5467cc4a85d3a9926ce62125fd12003d3c
Camilo Pinzon <pinzonac@gmail.com>
2024-04-05 00:52:16 -0500

    Partial styling

--- src/components/weatherComponent.tsx
+++ src/components/weatherComponent.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 
 import { fetchWeatherData } from "../api/weatherApi";
+import { LiaCloudShowersHeavySolid, LiaWindSolid } from "react-icons/lia";
+import { TiWeatherWindyCloudy } from "react-icons/ti";
+import { WiSolarEclipse } from "react-icons/wi";
+
+import "./weatherStyles.scss";
 
 const WeatherComponent = () => {
-    const [weatherData, setWeatherData] = useState<unknown | null>(null);
+	const [weatherData, setWeatherData] = useState<object | null>(null);
+	const [location, setLocation] = useState("New York");
 
-    const location = 'New York';
-
-    useEffect(() => {
+	/*useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await fetchWeatherData(location);
@@ -19,14 +23,70 @@ const WeatherComponent = () => {
         };
 
         fetchData();
-    }, []);
+    }, []);*/
 
-    return (
-        <div>
-            <p>{JSON.stringify(weatherData)}</p>
-            <p>{location}</p>
-        </div>
-    )
-}
+	return (
+		<main>
+			<section className="container">
+				<div className="location">
+					<div className="location__form">
+						<input type="text" name="city" id="city" value={location} />
+					</div>
+				</div>
+				<div className="weather">
+					<span className="weather__temperature">23°</span>
+					<span className="weather__condition">Showers</span>
+				</div>
+			</section>
+			<section className="container">
+				<div className="days">
+					<div className="container">
+						<h2>Today</h2>
+						<div className="row-container">
+							<div>
+								<LiaCloudShowersHeavySolid className="icon" />
+								<div className="left-aligned">
+									<div>23°</div>
+									<div>Showers</div>
+								</div>
+							</div>
+							<div className="wind-info">
+								<LiaWindSolid className="icon" />
+								<div>
+									<b>Wind</b>
+								</div>
+								<div className="left-aligned">
+									<div>Direction: North</div>
+									<div>Speed: 6.2 m/h</div>
+								</div>
+							</div>
+							<div className="atmosphere-info">
+								<TiWeatherWindyCloudy className="icon" />
+								<div>
+									<b>Atmosphere</b>
+								</div>
+								<div className="left-aligned">
+									<div>Humidity: 60%</div>
+									<div>Visibility: 10 km</div>
+									<div>Pressure: 1010 hPa</div>
+								</div>
+							</div>
+							<div className="astronomy-info">
+								<WiSolarEclipse className="icon" />
+								<div>
+									<b>Day n' Night</b>
+								</div>
+								<div className="left-aligned">
+									<div>Sunrise: 7:00 am</div>
+									<div>Sunset: 6:00pm</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
+};
 
 export default WeatherComponent;
